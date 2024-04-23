5e6572aaacd1c6e7ec55d43548a305511fd6bb79
Gabriel de Alencar <gabrieldealencar.dev@gmail.com>
2024-04-23 20:00:11 -0300

    ends the carte section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,27 @@
+const btnShowCarte = document.getElementById('btn-show-carte');
+const containerCarte = document.querySelector('.carte-overlay');
+const btnCloseCarte = document.getElementById('btn-close-carte');
+const body = document.querySelector('body');
+
+btnShowCarte.addEventListener('click', () => {
+    
+    containerCarte.style.opacity = 1;
+    containerCarte.style.pointerEvents = 'all';
+    body.style.overflowY = 'hidden';
+    
+});
+
+btnCloseCarte.addEventListener('click', () => {
+    
+    containerCarte.style.opacity = 0;
+    containerCarte.style.pointerEvents = 'none';
+    body.style.overflowY = 'scroll';
+
+});
+
+
+
+
 const swiper = new Swiper("#swiper-one", {
     spaceBetween: 15,
     pagination: {
